d3df706e80ddabdfbd5ec629d6e37ff35f39aaaf
Ewald Zietsman <ewald.zietsman@gmail.com>
2011-11-02 23:17:10 +0200

    All except Math Gr12 compiles, need to write a script to insert stuff into the PS books

---
+++ PhysicalScience/Grade12/build.sh
@@ -0,0 +1,5 @@
+latex --interaction nonstopmode -shell-escape PhysicalScience_Gr12.tex
+latex --interaction nonstopmode -shell-escape PhysicalScience_Gr12.tex
+latex --interaction nonstopmode -shell-escape PhysicalScience_Gr12.tex
+dvipdf PhysicalScience_Gr12.dvi
+
