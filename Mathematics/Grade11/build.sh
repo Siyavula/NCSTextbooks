114a2e1f084ca41120e69dc7b9dacc03b9e1ab6a
Ewald Zietsman <ewald@siyavula.org.za>
2011-11-12 00:34:21 +0200

    Final edits to Gr11 Maths NCS

--- Mathematics/Grade11/build.sh
+++ Mathematics/Grade11/build.sh
@@ -2,3 +2,4 @@ latex --interaction nonstopmode -shell-escape Mathematics_Gr11_Merged.tex
 latex --interaction nonstopmode -shell-escape Mathematics_Gr11_Merged.tex 
 latex --interaction nonstopmode -shell-escape Mathematics_Gr11_Merged.tex 
 dvipdf Mathematics_Gr11_Merged.dvi
+gs -q -sPAPERSIZE=a4 -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=NCSMathsGr11.pdf ../../covers/maths-gr11-front.pdf Mathematics_Gr11_Merged.pdf ../../covers/maths-gr11-back.pdf
