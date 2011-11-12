216b290145cfc83c8221296005856de86230c9aa
Ewald Zietsman <ewald@siyavula.org.za>
2011-11-13 00:04:52 +0200

    Layout edits

--- PhysicalScience/Grade11/build.sh
+++ PhysicalScience/Grade11/build.sh
@@ -2,4 +2,4 @@ latex --interaction nonstopmode -shell-escape PhysicalScience_Gr11.tex
 latex --interaction nonstopmode -shell-escape PhysicalScience_Gr11.tex
 latex --interaction nonstopmode -shell-escape PhysicalScience_Gr11.tex
 dvipdf PhysicalScience_Gr11.dvi
-
+gs -q -sPAPERSIZE=a4 -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=NCSScienceGr11.pdf ../../covers/science-gr11-front.pdf PhysicalScience_Gr11.pdf ../../covers/science-gr11-back.pdf
