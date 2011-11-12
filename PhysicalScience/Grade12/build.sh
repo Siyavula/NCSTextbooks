861e411da64c318ec84789e097021727f565fab9
Ewald Zietsman <ewald@siyavula.org.za>
2011-11-13 01:34:56 +0200

    Added frontmatter

--- PhysicalScience/Grade12/build.sh
+++ PhysicalScience/Grade12/build.sh
@@ -2,4 +2,5 @@ latex --interaction nonstopmode -shell-escape PhysicalScience_Gr12.tex
 latex --interaction nonstopmode -shell-escape PhysicalScience_Gr12.tex
 latex --interaction nonstopmode -shell-escape PhysicalScience_Gr12.tex
 dvipdf PhysicalScience_Gr12.dvi
+gs -q -sPAPERSIZE=a4 -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=NCSScienceGr12.pdf ../../covers/science-gr12-front.pdf PhysicalScience_Gr12.pdf ../../covers/science-gr12-back.pdf
 
